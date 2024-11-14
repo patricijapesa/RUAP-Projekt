dd02ea87e88fc4d52d68760af53c471f33e6786a
patricijapesa <patricija.pesa000@gmail.com>
2024-11-14 17:47:53 +0100

    Prvi commit

---
+++ RUAP_lv1/RUAP_lv1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RUAP_lv1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Patricije Peša!");
+        }
+    }
+}
